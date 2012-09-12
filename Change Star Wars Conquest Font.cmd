03e42dc5e019b89ec1dcdfab1ec601b6c0d0dedc
Swyter@Portatil <swyterzone@gmail.com>
2012-09-12 16:10:49 +0200

    *Simplified the font swap script.  Much nicer and talkative now. Tinier and less cluttered too.

--- Change Star Wars Conquest Font.cmd
+++ Change Star Wars Conquest Font.cmd
@@ -1,115 +1,37 @@
-rem  #############################################################################
-rem  # SCRIPT TO CHANGE THE FONT OF THE GAME ENGINE TO THE STAR WARS CONQUEST ONE
-rem  #############################################################################
-rem                      W W W . S W C O N Q U E S T . C O . C C 
-
-rem  Coded By Swyter | © 2010 All rights reserved
-rem ______________________________________________________________________________
-
+:: SCRIPT TO CHANGE THE FONT OF THE GAME ENGINE TO THE STAR WARS CONQUEST ONE
+:: coded by swyter | licensed under MIT like terms <http://opensource.org/licenses/mit-license.php>
 goto start
-exit
 
 :MB2SWC
-cls
+  @ren ..\..\Textures\Font.dds Font_original_mb_[modified_by_swc_script].dds > nul
+  @copy /Y .\Textures\Font_SWC.dds ..\..\Textures\Font.dds > nul
 
-@ren ..\..\Textures\Font.dds Font_original_mb_[modified_by_swc_script].dds > nul
-@copy /Y .\Textures\Font_SWC.dds ..\..\Textures\Font.dds > nul
-
-@ren ..\..\Data\Font_data.xml Font_data_original_mb_[modified_by_swc_script].xml > nul
-@copy /Y ".\Module Data\Font_data.xml" "..\..\Data\Font_data.xml" > nul
-
-echo. 
-echo. 
-echo   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo   ~           SCRIPT TO CHANGE THE STAR WARS CONQUEST FONT          ~
-echo   ~           coded by Swyter                                       ~
-echo   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo. 
-echo. 
-echo. 
-echo   Press Enter to replace the MountandBlade default font to the SWC one
-echo   If the font is already changed, the script will restore it.
-echo.  You can exit anytime pressing Ctrl+C..
-echo. 
-echo. 
-echo.   [X] Progress: Done, Now you have the SWC font...
-echo.   [!] Press Enter again to close the SWC script window
-pause > nul
-rem #That's for waiting a second to show the message ;D
-echo. 
-echo Good Bye! Enjoy the Game and greetings from Swyter :D
-ping -n 2 127.0.0.1 > nul
-exit
-
-
+  @ren ..\..\Data\Font_data.xml Font_data_original_mb_[modified_by_swc_script].xml > nul
+  @copy /Y ".\Module Data\Font_data.xml" "..\..\Data\Font_data.xml" > nul
+  goto :eof
 
 :SWC2MB
+  @del /F /Q ..\..\Textures\Font.dds > nul
+  @ren ..\..\Textures\Font_original_mb_[modified_by_swc_script].dds Font.dds > nul
 
-cls
-
-@del /F /Q ..\..\Textures\Font.dds > nul
-@ren ..\..\Textures\Font_original_mb_[modified_by_swc_script].dds Font.dds > nul
-
-@del /F /Q ..\..\Data\Font_data.xml > nul
-@ren ..\..\Data\Font_data_original_mb_[modified_by_swc_script].xml Font_data.xml > nul
-
-echo. 
-echo. 
-echo   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo   ~           SCRIPT TO CHANGE THE STAR WARS CONQUEST FONT          ~
-echo   ~           coded by Swyter                                       ~
-echo   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo. 
-echo. 
-echo. 
-echo   Press Enter to replace the MountandBlade default font to the SWC one
-echo   If the font is already changed, the script will restore it.
-echo.  You can exit anytime pressing Ctrl+C..
-echo. 
-echo. 
-echo.   [X] Progress: Done, Now you have the MB default font again...
-echo. 
-echo.   [!] Press Enter to close the SWC script window
-pause > nul
-rem # That's for wait a second to show the message ;D
-echo. 
-echo Good Bye! Enjoy the Game and greetings from Swyter :D
-ping -n 2 127.0.0.1 > nul
-exit
-
+  @del /F /Q ..\..\Data\Font_data.xml > nul
+  @ren ..\..\Data\Font_data_original_mb_[modified_by_swc_script].xml Font_data.xml > nul
+  goto :eof
 
 
 :start
-echo off
-cls
-color 0E
-title Star Wars Conquest Font Script - Coded by Swyter
+  @echo off && cls && color 0E
+  title Star Wars Conquest Font Script - Coded by Swyter
 
-echo. 
-echo. 
-echo   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo   ~           SCRIPT TO CHANGE THE STAR WARS CONQUEST FONT          ~
-echo   ~           coded by Swyter                                       ~
-echo   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo. 
-echo. 
-echo. 
-echo   Press Enter to replace the MountandBlade default font to the SWC one
-echo   If the font is already changed, the script will restore it.
-echo.  You can exit anytime pressing Ctrl+C..
-echo. 
-echo. 
-echo.   [ ] Progress: Waiting for Enter key
-pause > nul
-echo. 
-echo. 
-if exist ..\..\Textures\Font_original_mb_[modified_by_swc_script].dds (
 
-goto SWC2MB
-
-) else (
-
-goto MB2SWC
-
-)
-
+  if not exist ..\..\Textures\Font_original_mb_[modified_by_swc_script].dds (
+      call :MB2SWC
+      echo ^| Now you're using our own Star Wars Conquest font.
+      echo ^| Run this script again to bring the original back.
+  ) else (
+      call :SWC2MB
+      echo ^| Now you're using the original Mount^&Blade font again.
+      echo ^| Run this script once more if you want to use our own.
+  )
+  pause
+  exit
