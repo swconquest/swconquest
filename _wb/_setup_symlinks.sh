3029299d22501a418d40a1ffd7e851c195e2d078
Swyter <swyterzone@gmail.com>
2016-08-05 11:41:00 +0200

    *start warband porting process. welcome back!  - add _wb subfolder, mirroring my tld structure.  - add symlinking script.

---
+++ _wb/_setup_symlinks.sh
@@ -0,0 +1,16 @@
+mkdir Data
+mkdir Textures
+
+ln -s ../main.bmp .
+ln -s ../module.ini .
+
+ln -s ../Languages languages
+ln -s ../Music .
+ln -s ../Resource .
+ln -s ../SceneObj .
+ln -s ../Sounds .
+
+ln -s ../Textures/* Textures/
+
+ln -s ../Font/FONT_DATA.XML Data/font_data.xml
+ln -s ../Font/FONT_SWC.dds  Textures/font.dds
