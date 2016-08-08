798dfb1f85fcf36be4a5ef60f8e1568267a9dba9
Swyter <swyterzone@gmail.com>
2016-08-09 00:05:10 +0200

    *this should be it, adding proper branching and ffp fog emulation should make it better.

--- _wb/GLShaders/fs_swconquest_galaxy.glsl
+++ _wb/GLShaders/fs_swconquest_galaxy.glsl
@@ -1,3 +1,5 @@
+/* swyter -- custom swc glsl shaders for warband, ported from hlsl by hand */
+
 uniform sampler2D diffuse_texture;
 uniform sampler2D diffuse_texture_2;
 uniform vec4 vFogColor;
@@ -10,27 +12,31 @@ void main ()
 {
   vec4 Output_RGBColor;
   vec4 tex_col = texture2D(diffuse_texture, Tex0);
-
-  tex_col.xyz = pow(tex_col.xyz, vec3(2.2, 2.2, 2.2));
-
-  if(tex_col.a < 0.11)
-    discard;
+       tex_col.xyz = pow(tex_col.xyz, vec3(2.2));
 
   Output_RGBColor = Color * tex_col;
 
-  vec4 tex_colb = texture2D(diffuse_texture_2, Tex0 * 30.0);
-  vec4 tex_colc = texture2D(diffuse_texture_2, Tex0 * 20.0);
+  if ((1.0 - Tex0.y) < 0.86)
+  {
+      if (tex_col.a < 0.11)
+        discard;
 
-  Output_RGBColor.rg  += tex_colc.rg * 4.0;
-  Output_RGBColor.rgb *= tex_colb.rgb * 1.5 + (tex_col.rgb / 14.0);
-  Output_RGBColor.rgb *= clamp((Tex0.x + 0.5) / (Tex0.y + 0.5), 0.0, 1.0);
+      vec4 tex_colb = texture2D(diffuse_texture_2, Tex0 * 30.0);
+      vec4 tex_colc = texture2D(diffuse_texture_2, Tex0 * 20.0);
 
-  Output_RGBColor.rgb *= tex_col.r + tex_col.g;
+      Output_RGBColor.rg  += tex_colc.rg * 4.0;
+      Output_RGBColor.rgb *= tex_colb.rgb * 1.5 + (tex_col.rgb / 14.0);
+      Output_RGBColor.rgb *= clamp((Tex0.x + 0.5) / (Tex0.y + 0.5), 0.0, 1.0);
 
-  Output_RGBColor.a    = max(tex_colb.a, Output_RGBColor.a / 2.0);
-  Output_RGBColor.a   /= tex_colb.r;
+      Output_RGBColor.rgb *= tex_col.r + tex_col.g;
 
+      Output_RGBColor.a    = max(tex_colb.a, Output_RGBColor.a / 2.0);
+      Output_RGBColor.a   /= tex_colb.r;
+  }
+
+  /* gamma correction and fixed-function fog emulation */
   Output_RGBColor.rgb = pow(Output_RGBColor.rgb, vec3(output_gamma_inv));
+  Output_RGBColor.rgb = mix(vFogColor.xyz, Output_RGBColor.rgb, Fog);
 
   gl_FragColor = Output_RGBColor;
 }
