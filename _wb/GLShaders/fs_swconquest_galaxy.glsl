16284f7033268144a0cea63d522288ea7de182fb
Swyter <swyterzone@gmail.com>
2016-08-08 23:30:35 +0200

    *implement initial galaxy GLSL shaders.

--- _wb/GLShaders/fs_swconquest_galaxy.glsl
+++ _wb/GLShaders/fs_swconquest_galaxy.glsl
@@ -1,4 +1,5 @@
 uniform sampler2D diffuse_texture;
+uniform sampler2D diffuse_texture_2;
 uniform vec4 vFogColor;
 uniform vec4 output_gamma_inv;
 varying float Fog;
@@ -7,15 +8,30 @@ varying vec2 Tex0;
 varying vec4 SunLight;
 void main ()
 {
-  vec4 tex_col_1;
-  vec4 tmpvar_2;
-  vec4 tmpvar_3;
-  tmpvar_3 = texture2D (diffuse_texture, Tex0);
-  tex_col_1.w = tmpvar_3.w;
-  tex_col_1.xyz = pow (tmpvar_3.xyz, vec3(2.2, 2.2, 2.2));
-  tmpvar_2 = (tex_col_1 * (Color + SunLight));
-  tmpvar_2.xyz = pow (tmpvar_2.xyz, output_gamma_inv.xyz);
-  tmpvar_2.xyz = mix (vFogColor.xyz, tmpvar_2.xyz, Fog);
-  gl_FragColor = tmpvar_2;
+  vec4 Output_RGBColor;
+  vec4 tex_col = texture2D(diffuse_texture, Tex0);
+
+  tex_col.xyz = pow(tex_col.xyz, vec3(2.2, 2.2, 2.2));
+
+  if(tex_col.a < 0.11)
+    discard;
+
+  Output_RGBColor = Color * tex_col;
+
+  vec4 tex_colb = texture2D(diffuse_texture_2, Tex0 * 30.0);
+  vec4 tex_colc = texture2D(diffuse_texture_2, Tex0 * 20.0);
+
+  Output_RGBColor.rg  += tex_colc.rg * 4.0;
+  Output_RGBColor.rgb *= tex_colb.rgb * 1.5 + (tex_col.rgb / 14.0);
+  Output_RGBColor.rgb *= clamp((Tex0.x + 0.5) / (Tex0.y + 0.5), 0.0, 1.0);
+
+  Output_RGBColor.rgb *= tex_col.r + tex_col.g;
+
+  Output_RGBColor.a    = max(tex_colb.a, Output_RGBColor.a / 2.0);
+  Output_RGBColor.a   /= length(tex_colb.rgb);
+
+  Output_RGBColor.rgb = pow(Output_RGBColor.rgb, vec3(output_gamma_inv));
+
+  gl_FragColor = Output_RGBColor;
 }
 
--- _wb/GLShaders/vs_swconquest_galaxy.glsl
+++ _wb/GLShaders/vs_swconquest_galaxy.glsl
@@ -20,58 +20,24 @@ attribute vec2 inTexCoord;
 varying float Fog;
 varying vec4 Color;
 varying vec2 Tex0;
-varying vec4 SunLight;
-varying vec4 ShadowTexCoord;
 void main ()
-{
-  vec4 tmpvar_1;
-  tmpvar_1.w = 1.0;
-  tmpvar_1.xyz = inPosition;
-  vec4 diffuse_light_2;
-  vec4 tmpvar_3;
-  vec4 tmpvar_4;
-  tmpvar_3 = (matWorldViewProj * tmpvar_1);
-  vec4 tmpvar_5;
-  tmpvar_5.w = 0.0;
-  tmpvar_5.xyz = inNormal;
-  vec3 tmpvar_6;
-  tmpvar_6 = normalize((matWorld * tmpvar_5).xyz);
-  diffuse_light_2 = (vAmbientColor + inColor1.zyxw);
-  diffuse_light_2.xyz = (diffuse_light_2.xyz + (clamp (
-    dot (tmpvar_6, -(vSkyLightDir.xyz))
-  , 0.0, 1.0) * vSkyLightColor.xyz));
-  vec4 vWorldPos_7;
-  vWorldPos_7 = (matWorld * tmpvar_1);
-  vec3 vWorldN_8;
-  vWorldN_8 = tmpvar_6;
-  vec4 total_10;
-  total_10 = vec4(0.0, 0.0, 0.0, 0.0);
-  for (int j_9 = 0; j_9 < iLightPointCount; j_9++) {
-    int tmpvar_11;
-    tmpvar_11 = iLightIndices[j_9];
-    vec3 tmpvar_12;
-    tmpvar_12 = (vLightPosDir[tmpvar_11].xyz - vWorldPos_7.xyz);
-    total_10 = (total_10 + ((
-      clamp (dot (vWorldN_8, normalize(tmpvar_12)), 0.0, 1.0)
-     * vLightDiffuse[tmpvar_11]) * (1.0/(
-      dot (tmpvar_12, tmpvar_12)
-    ))));
-  };
-  diffuse_light_2 = (diffuse_light_2 + total_10);
-  vec4 tmpvar_13;
-  tmpvar_13.w = 1.0;
-  tmpvar_13.xyz = vSunColor.xyz;
-  vec3 tmpvar_14;
-  tmpvar_14 = (matWorldView * tmpvar_1).xyz;
-  gl_Position = tmpvar_3;
-  Fog = (1.0/(exp2((
-    sqrt(dot (tmpvar_14, tmpvar_14))
-   * fFogDensity))));
-  Color = ((vMaterialColor * inColor0.zyxw) * diffuse_light_2);
-  Tex0 = inTexCoord;
-  SunLight = (((
-    clamp (dot (tmpvar_6, -(vSunDir.xyz)), 0.0, 1.0)
-   * tmpvar_13) * vMaterialColor) * inColor0.zyxw);
-  ShadowTexCoord = tmpvar_4;
+{ 
+	gl_Position = matWorldViewProj * vec4(inPosition.xyz, 1.0);
+
+	vec3 P = (matWorldView * vec4(inPosition, 1.0)).xyz; //position in view space
+
+	//apply fog
+	float d = length(P);
+
+	//float3 U = mul(matWorld, inPosition).xyz;
+	//float  u = length(U.z); //Exponential HeightFog! :)
+	Fog = 1.0; //get_fog_amount(d,u);
+
+	
+	Tex0 = inTexCoord;
+	Color = inColor0 * vMaterialColor;
+	Color.b += (d + 0.2);
+	Color.a  = 0.8;
+  
 }
 
