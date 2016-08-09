677458134c505d850bebf5eb2e8c1217aafbd7fb
Swyter <swyterzone@gmail.com>
2016-08-09 16:22:47 +0200

    *add basic GLSL glow shader.

--- _wb/GLShaders/fs_swconquest_glow.glsl
+++ _wb/GLShaders/fs_swconquest_glow.glsl
@@ -9,11 +9,20 @@ void main ()
 {
   vec4 tex_col_1;
   vec4 tmpvar_2;
-  vec4 tmpvar_3;
-  tmpvar_3 = texture2D (diffuse_texture, Tex0);
-  tex_col_1.w = tmpvar_3.w;
-  tex_col_1.xyz = pow (tmpvar_3.xyz, vec3(2.2, 2.2, 2.2));
-  tmpvar_2 = (tex_col_1 * (Color + SunLight));
+  vec4 tex_col;
+  tex_col = texture2D (diffuse_texture, Tex0);
+  tex_col_1.w = tex_col.w;
+  tex_col_1.xyz = pow (tex_col.xyz, vec3(2.2, 2.2, 2.2));
+
+  // --
+
+  vec4 In_SunLight = max(tex_col.aaaa, SunLight);
+  tmpvar_2 = tex_col *   Color   * (In_SunLight);
+
+  //tmpvar_2 = (tex_col_1 * (Color + SunLight));
+
+  // --
+
   tmpvar_2.xyz = pow (tmpvar_2.xyz, output_gamma_inv.xyz);
   tmpvar_2.xyz = mix (vFogColor.xyz, tmpvar_2.xyz, Fog);
   gl_FragColor = tmpvar_2;
