55cb80e679b679a8817edd0d35ffe149d46af8eb
Swyter <swyterzone@gmail.com>
2016-08-19 20:57:52 +0200

    *add a bash script to strip out dev stuff. improve workshop buildbot yaml script.

---
+++ _wb/_strip_unused.sh
@@ -0,0 +1,19 @@
+rm -f  ./languages/*
+rm -rf ./languages/.tx
+
+rm -f  ./Music/Readme.txt
+rm -rf ./Music/_*
+
+rm -f  ./SceneObj/*.exe
+rm -rf ./SceneObj/_*
+
+rm -rf ./Sounds/_*
+
+rm -rf ./Textures/_*
+
+rm -rf ./_*
+
+rm -f  ./*.bat
+rm -f  ./*.exe
+rm -f  ./*.h
+rm -f  ./*src*
