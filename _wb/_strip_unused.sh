4791b41fd6c30f48f269df052596a5bfa7dcb242
Swyter <swyterzone@gmail.com>
2016-08-21 14:14:11 +0200

    *make the build scripts do it all the way around, copy the _wb folder over vanilla and clean it up. just like TLD, hopefully with less pitfalls and headaches. !workshop

--- _wb/_strip_unused.sh
+++ _wb/_strip_unused.sh
@@ -8,14 +8,28 @@ rm -f  ./SceneObj/*.exe
 rm -rf ./SceneObj/_*
 
 rm -rf ./Sounds/_*
+rm -f  ./Sounds/Readme.txt
 
 rm -rf ./Textures/_*
+rm -f  ./Textures/Readme.txt
 
 rm -rf ./Resource/_*
 
 rm -f  ./*.bat
+rm -f  ./*.cmd
 rm -f  ./*.exe
 rm -f  ./*.h
 rm -f  ./*src*
+rm -f  ./*.ico
+rm -f  ./*.pdf
+rm -f  ./*.odt
+rm -f  ./*.psd
+rm -f  ./.*
+rm -f  ./*.yml
+
+rm -rf 'Artwork by Giordano'
+rm -rf 'Artwork by Ornicar'
+rm -rf 'Extras'
+rm -rf 'Font'
 
 rm -rf ./_*
